d3a4f2ef57627e52b582938af548168ad8c3163d
Pedja Trifunov <pedjat@nordeus.com>
2023-09-11 18:01:18 +0200

    Implemented simple recursive dir walk

--- main/main.go
+++ main/main.go
@@ -1,72 +1,33 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path"
+)
 
 func main() {
-	dirLister := NewDirLister()
-	dirLister.List(".")
-	dirLister.WaitToFinish()
+	ListDirectoryRecursivelyParallel("../..")
 }
 
-type DirLister struct {
-	workers              []Worker
-	writeOutWorkerInput  chan os.DirEntry
-	writeOutWorkerOutput chan bool
-}
+func ListDirectoryRecursivelyParallel(baseDir string) {
+	dirsToProcess := []string{baseDir}
 
-type Worker struct {
-	inputChanel  chan string
-	outputChanel chan os.DirEntry
-}
+	for len(dirsToProcess) > 0 {
+		dir := dirsToProcess[0]
+		dirsToProcess = dirsToProcess[1:]
 
-func (w *Worker) GetInputChanel() chan string {
-	w.init()
-	return w.inputChanel
-}
+		dirContents, err := os.ReadDir(dir)
+		if err != nil {
+			panic(err)
+		}
 
-func (w *Worker) init() {
-	if w.inputChanel != nil {
-		return
+		for _, singleDir := range dirContents {
+			singleDirName := path.Join(dir, singleDir.Name())
+			fmt.Println(singleDirName)
+			if singleDir.IsDir() {
+				dirsToProcess = append(dirsToProcess, singleDirName)
+			}
+		}
 	}
-
-	w.inputChanel = make(chan string)
-	w.outputChanel = make(chan os.DirEntry)
-}
-
-func (w *Worker) GetOutputChanel() chan os.DirEntry {
-	w.init()
-	return w.outputChanel
-}
-
-func (o *DirLister) List(basePath string) {
-	for i := 0; i < 5; i++ {
-		worker := Worker{}
-		o.workers = append(o.workers, worker)
-		go dirListerWorker(worker.GetInputChanel(), worker.GetOutputChanel())
-	}
-
-	o.writeOutWorkerInput = make(chan os.DirEntry)
-	o.writeOutWorkerOutput = make(chan bool)
-	go dirWriteOutWorker(o.writeOutWorkerInput, o.writeOutWorkerOutput)
-
-	go o.listImpl(basePath)
-}
-
-func dirWriteOutWorker(input chan os.DirEntry, output chan bool) {
-}
-
-func (o *DirLister) WaitToFinish() {
-
-}
-
-func (o *DirLister) listImpl(path string) {
-
-}
-
-func NewDirLister() DirLister {
-	return DirLister{}
-}
-
-func dirListerWorker(chan string, chan os.DirEntry) {
-
 }
